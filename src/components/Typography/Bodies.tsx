0e6319a2613a1fec1014f5c32752fa1408331d33
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-14 14:01:46 +0700

    add bodies styles

---
+++ src/components/Typography/Bodies.tsx
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+const BodyM = css`
+  font-weight: 300;
+  font-size: 15px;
+`;
+
+const BodyS = css`
+  font-weight: 300;
+  font-size: 13px;
+`;
+
+export { BodyM, BodyS };
