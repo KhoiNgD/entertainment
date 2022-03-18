f2181dbf9dfbccc3e925d3c5cc6a201cc61f0333
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-18 14:50:22 +0700

    show data bookmarked page

--- src/pages/Bookmarked/Bookmarked.tsx
+++ src/pages/Bookmarked/Bookmarked.tsx
@@ -1,14 +1,35 @@
-type Props = {};
+import { GridThumbnails } from "components/GridThumbnails";
+import { SearchField } from "components/SearchField";
+import { useData } from "context/data-context";
+import styled from "styled-components";
 
-function Bookmarked({}: Props) {
+function Bookmarked() {
+  const [data] = useData();
+  const bookmarkedMovies = data
+    .filter((item) => item.category === "Movie" && item.isBookmarked)
+    .map((item) => ({ ...item, isTrending: false }));
+  const bookmarkedTVSeries = data
+    .filter((item) => item.category === "TV Series" && item.isBookmarked)
+    .map((item) => ({ ...item, isTrending: false }));
+
   return (
-    <div>
-      Search for bookmarked shows Bookmarked Movies
-      {/* <!-- Display bookmarked movies --> */}
-      Bookmarked TV Series
-      {/* <!-- Display bookmarked TV series --> */}
-    </div>
+    <>
+      <SearchField placeholder="Search for bookmarked shows" />
+      <Wrapper>
+        <GridThumbnails data={bookmarkedMovies} heading="Bookmarked Movies" />
+        <GridThumbnails
+          data={bookmarkedTVSeries}
+          heading="Bookmarked TV Series"
+        />
+      </Wrapper>
+    </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
 
 export { Bookmarked };
